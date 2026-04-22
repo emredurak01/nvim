241dbe76aa01260587be1d5d9d660edd8a43fe3a
Emre Durak <emredurak01@gmail.com>
2026-04-22 12:41:46 +0200

    nvim-lint

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -93,6 +93,26 @@ return {
   },
 
   {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPost", "BufWritePost" },
+    config = function()
+      local lint = require "lint"
+      lint.linters_by_ft = {
+        lua = { "luacheck" },
+        sh = { "shellcheck" },
+        yaml = { "yamllint" },
+        markdown = { "markdownlint" },
+        go = { "golangcilint" },
+      }
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+
+  {
     "folke/trouble.nvim",
     opts = {
       focus = true,
